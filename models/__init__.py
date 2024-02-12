b2a3cd98df617284d7be5327e3383f98356b6037
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-12 05:38:16 +0300

    Removing the first try

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""this files includes files in this directory to convert them as a package"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
